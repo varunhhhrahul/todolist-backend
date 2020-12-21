6557564c64f397c3c5ed46c70f24e492d5b5a934
Varun Ramnani <varunhhhrahul@gmail.com>
2020-12-21 16:48:51 +0530

    added function to connect to database

---
+++ functions/config/db.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const { MONGO_URI } = require('../config/config');
+const connectDB = async () => {
+  const conn = await mongoose.connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  });
+
+  console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold);
+};
+
+module.exports = connectDB;
